4b1fbfccbc556847a55bb88826315c55580a9fd8
Craghorn <Craghorn@gmail.com>
2022-10-03 17:35:28 +0300

    Data flow pt2

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class MainManager : MonoBehaviour
 
     public static MainManager Instance;
 
-    public Color TeamColor; //new variable color for forklifts
+    public Color TeamColor; // new variable color for forklifts
 
     private void Awake()
     {
@@ -22,5 +23,34 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadColor(); // Load color at start
+    }
+
+    [System.Serializable]
+    class SaveData // new class for saving color
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor() // new method for saving color
+    {
+        SaveData data = new SaveData(); // new object
+        data.TeamColor = TeamColor; // write MainManager color in new object variable
+
+        string json = JsonUtility.ToJson(data); // transform data object to JSON
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json); // save file (good link forlists the actual paths per platform https://docs.unity3d.com/ScriptReference/Application-persistentDataPath.html)
+    }
+
+    public void LoadColor() // new method for loading color
+    {
+        string path = Application.persistentDataPath + "/savefile.json"; // variable of path of file
+        if (File.Exists(path)) // method Exists to check existence of file
+        {
+            string json = File.ReadAllText(path); // reading to string
+            SaveData data = JsonUtility.FromJson<SaveData>(json);  // converted json to data object
+
+            TeamColor = data.TeamColor; // MainManager TeamColor now is from saved data object color from json
+        }
     }
 }
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -16,15 +16,16 @@ public class MenuUIHandler : MonoBehaviour
 
     public void NewColorSelected(Color color)
     {
-        // add code here to handle when a color is selected
+        // Add code here to handle when a color is selected
         MainManager.Instance.TeamColor = color;
     }
     
     private void Start()
     {
         ColorPicker.Init();
-        //this will call the NewColorSelected function when the color picker have a color button clicked.
+        // This will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor); // Line will pre-select the saved color in the MainManager
     }
 
     public void StartNew()
@@ -34,10 +35,23 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
+        MainManager.Instance.SaveColor(); // Save the user’s last selected color when the application exits
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
         Application.Quit();
 #endif
+    }
+
+    // Methods for buttons
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 }
